ca59fcd2619756c6295a59d727d65cc86c67689e
William Stadtlander <wastadtlander@vt.edu>
2023-04-11 17:00:29 -0400

    i made magic happen

--- WineClassifier.py
+++ WineClassifier.py
@@ -8,17 +8,20 @@ import statistics
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from cnn import cnn
 
 
 # constants
 ORIGINAL_DATASET = "WineQT.xlsx"
 
-ORIGINAL_DATA_COLUMNS = { "fixed acidity": float,	"volatile acidity": float,	"citric acid": float,	"residual sugar": float,
-                    "chlorides": float,	"free sulfur dioxide": float,	"total sulfur dioxide": float,	"density": float,	"pH": float,
-                    "sulphates": float,	"alcohol": float,	"quality": float, "Id": float}
+ORIGINAL_DATA_COLUMNS = {"fixed acidity": float,	"volatile acidity": float,	"citric acid": float,	"residual sugar": float,
+                         "chlorides": float,	"free sulfur dioxide": float,	"total sulfur dioxide": float,	"density": float,	"pH": float,
+                         "sulphates": float,	"alcohol": float,	"quality": float, "Id": float}
 
 # Function to load dataframe from excel
-def getData(path = ORIGINAL_DATASET):
+
+
+def getData(path=ORIGINAL_DATASET):
 
     if not os.path.exists(path):
         print("[ERROR] COIN COLOR DATA FILE NOT FOUND")
@@ -33,14 +36,14 @@ def main():
 
     # Load data from excel
     df = getData()
-    #split into features and results
+    # split into features and results
     X = np.array(df.iloc[:, 0:11].values)
     y = np.array(df.iloc[:, 11].values)
     # split the dataset into a training and testing portion for validation purposes
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-    ## TODO ## Implement ML Algorithms in their own functions as seen with getData() above and call them below
-
+    # TODO ## Implement ML Algorithms in their own functions as seen with getData() above and call them below
+    print(cnn())
 
     print("\n[INFO] ENDING MAIN FUNCTION\n")    # program status update
 
